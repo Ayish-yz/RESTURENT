e2cf91416cfc68ea4aa6d96ead0378ac8d9586c3
Ayish-yz <ayshaaazzzzz@gmail.com>
2024-08-06 16:22:32 +0530

    Add files via upload

---
+++ mainn.js
@@ -0,0 +1,49 @@
+(function ($){
+    "use strict ";
+
+    // wow js
+    new WOW().init();   
+
+    // counter up
+    $('[data-toggle="counter-up]').counterUp({
+        delay:10,
+        time:2000
+    });
+
+    //testimonial-corousel--
+    $(".testimonial-corousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots:true,
+        loop: true,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            768: {
+                items:2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+
+    //back to top
+    $(window).scroll(function(){
+        if($(this).scrollTop()> 300){
+            $(' .back-to-top').fadeIn('slow');
+        }else{
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+
+    $('.back-to-top').click(function (){
+        $('html, body').animate({scrollTop: 0}, 100, 'easeInOutExpo');
+        return false;
+    });
+
+} )(jQuery);
